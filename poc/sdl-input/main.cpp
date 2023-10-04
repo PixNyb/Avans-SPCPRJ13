bd7808b85f408d5a74400255816f29dc42ce4324
PixNyb <contact@roelc.me>
2023-10-04 21:36:32 +0200

    Added fps count, made movement better

--- poc/sdl-input/main.cpp
+++ poc/sdl-input/main.cpp
@@ -1,9 +1,12 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <vector>
 
-const int VELOCITY = 10;
+const int VELOCITY = 1;
+std::vector<int> fpsCounts;
 
 int main(int argc, char *argv[]) {
   // Initialize SDL
@@ -44,6 +47,14 @@ int main(int argc, char *argv[]) {
   bool isHovering = false;
   Uint32 lastBackgroundColorChangeTime = SDL_GetTicks();
 
+  // Initialize the arrow key state array
+  bool arrowKeys[4] = {false, false, false, false};
+
+  // Initialize the fps counter variables
+  Uint32 lastFrameTime = SDL_GetTicks();
+  int frameCount = 0;
+  int fps = 0;
+
   while (!quit) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
@@ -57,22 +68,38 @@ int main(int argc, char *argv[]) {
           quit = true;
           break;
         case SDLK_UP:
-          squareY -= VELOCITY;
+          arrowKeys[0] = true;
           break;
         case SDLK_DOWN:
-          squareY += VELOCITY;
+          arrowKeys[1] = true;
           break;
         case SDLK_LEFT:
-          squareX -= VELOCITY;
+          arrowKeys[2] = true;
           break;
         case SDLK_RIGHT:
-          squareX += VELOCITY;
+          arrowKeys[3] = true;
           break;
         case SDLK_SPACE:
           squareColor.r = std::rand() % 256;
           squareColor.g = std::rand() % 256;
           squareColor.b = std::rand() % 256;
           previousSquareColor = squareColor;
+          break;
+        }
+        break;
+      case SDL_KEYUP:
+        switch (event.key.keysym.sym) {
+        case SDLK_UP:
+          arrowKeys[0] = false;
+          break;
+        case SDLK_DOWN:
+          arrowKeys[1] = false;
+          break;
+        case SDLK_LEFT:
+          arrowKeys[2] = false;
+          break;
+        case SDLK_RIGHT:
+          arrowKeys[3] = false;
           break;
         }
         break;
@@ -106,15 +133,60 @@ int main(int argc, char *argv[]) {
                            backgroundColor.b, backgroundColor.a);
     SDL_RenderClear(renderer);
 
+    // Update the position of the square based on the arrow key state
+    if (arrowKeys[0]) {
+      squareY -= VELOCITY;
+    }
+    if (arrowKeys[1]) {
+      squareY += VELOCITY;
+    }
+    if (arrowKeys[2]) {
+      squareX -= VELOCITY;
+    }
+    if (arrowKeys[3]) {
+      squareX += VELOCITY;
+    }
+
     // Draw the square with the current color
     SDL_SetRenderDrawColor(renderer, squareColor.r, squareColor.g,
                            squareColor.b, squareColor.a);
     SDL_Rect squareRect = {squareX, squareY, squareSize, squareSize};
     SDL_RenderFillRect(renderer, &squareRect);
 
+    // Update the fps counter
+    frameCount++;
+    Uint32 currentFrameTime = SDL_GetTicks();
+    if (currentFrameTime - lastFrameTime >= 1000) {
+      fps = frameCount;
+      frameCount = 0;
+      lastFrameTime = currentFrameTime;
+    }
+
+    // Log the FPS every 5 seconds
+    static Uint32 lastFpsLogTime = SDL_GetTicks();
+    static Uint32 lastFpsStoreTime = SDL_GetTicks();
+
+    if (currentFrameTime - lastFpsStoreTime >= 1000) {
+      if (currentFrameTime - lastFpsLogTime >= 5000) {
+        std::cout << "FPS: " << fps << std::endl;
+        lastFpsLogTime = currentFrameTime;
+      }
+
+      fpsCounts.push_back(fps);
+      lastFpsStoreTime = currentFrameTime;
+    }
+
     SDL_RenderPresent(renderer);
     SDL_Delay(1);
   }
+
+  // Log the average FPS, add up all the FPS counts and divide by the number of
+  // counts
+  int fpsTotal = 0;
+  for (int fpsCount : fpsCounts) {
+    fpsTotal += fpsCount;
+  }
+  std::cout << "Average FPS: " << fpsTotal / fpsCounts.size() << std::endl;
 
   // Clean up and exit
   SDL_DestroyRenderer(renderer);
