289f5bbeeb015e2d25e385575b7acf76d143552e
RobinPijnappelsAvans <73393404+RobinPijnappelsAvans@users.noreply.github.com>
2023-11-08 13:20:50 +0100

    Doxyen documentation added

--- include/project/core/events/event.hpp
+++ include/project/core/events/event.hpp
@@ -1,27 +1,42 @@
-//
-// Created by Robin Pijnappels on 08/11/2023.
-//
+/**
+ * @file event.hpp
+ * @author Robin Pijnappels
+ * @brief This file contains the Event class definition.
+ * @version 0.1
+ * @date 2023-11-08
+ *
+ * The Event class is a base class for all events within the game engine. It holds the
+ * type of the event, and specific event classes can extend it to provide additional
+ * information relevant to that event type.
+ *
+ */
 
 #ifndef DEFUNBOBENGINE_EVENT_HPP
 #define DEFUNBOBENGINE_EVENT_HPP
 
-enum class EventType {
-    None,
-    KeyPress,
-    KeyRelease,
-    MouseButtonDown,
-    MouseButtonUp,
-    MouseMove,
-    WindowClose,
-};
+#include "event_type.hpp"
 
+/**
+ * @class Event
+ * @brief Base class for events in the game engine.
+ *
+ * Event serves as a base class for all types of events. It contains a member that
+ * specifies the type of event, allowing event handlers to process it accordingly.
+ */
 class Event {
-        public:
-        EventType type;
+public:
+    EventType type; ///< The type of the event.
 
-        Event(EventType type) : type(type) {}
+    /**
+     * @brief Constructs an event with a specified type.
+     * @param type The type of the event.
+     */
+    Event(EventType type) : type(type) {}
 
-        virtual ~Event() {}
+    /**
+     * @brief Virtual destructor for polymorphic deletion.
+     */
+    virtual ~Event() {}
 };
 
 #endif //DEFUNBOBENGINE_EVENT_HPP
