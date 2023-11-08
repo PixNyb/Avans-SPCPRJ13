21b6e8389ff08e6ba99147552fa6b622f832b14f
Melvin <17225342+ChocoMelvin@users.noreply.github.com>
2023-11-08 14:59:14 +0100

    cmake change and base menu scene

--- src/project/scenes/specializations/base_menu_scene.cpp
+++ src/project/scenes/specializations/base_menu_scene.cpp
@@ -10,3 +10,20 @@
  */
 
 #include "base_menu_scene.hpp"
+
+BaseMenuScene::BaseMenuScene()
+{
+}
+
+BaseMenuScene::~BaseMenuScene()
+{
+}
+
+void BaseMenuScene::AddText(const std::string &text, const Point &position, const int size, const std::string &font){}
+
+std::shared_ptr<Button> BaseMenuScene::AddButton(const std::string &text, const Point &position, const Color &bgColor, const Color &textColor, const int textSize, const std::string &font){
+    return {};
+}
+std::shared_ptr<Button> BaseMenuScene::AddButton(const std::string &text, const Point &position, const std::string &buttonBackgroundImagePath, const int textSize, const std::string &font){
+    return {};
+}
