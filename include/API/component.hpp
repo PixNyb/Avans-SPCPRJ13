b3c41559f146ef63647b6b4d8267d4eb11c7d164
PixNyb <contact@roelc.me>
2023-11-08 11:24:50 +0100

    Add Component class with active flag and methods

--- include/API/component.hpp
+++ include/API/component.hpp
@@ -12,13 +12,63 @@
 #ifndef AVANS_SPCPRJ13_COMPONENT_H
 #define AVANS_SPCPRJ13_COMPONENT_H
 
+/**
+ * @class Component
+ * @brief The Component class is a base class for all components that can be
+ * added to a GameObject.
+ *
+ * It contains an active flag and methods for getting/setting the active status,
+ * updating the component, and checking a condition.
+ */
 class Component {
 protected:
-  bool active;
+  bool active; ///< The active flag of the Component.
 
 public:
+  /**
+   * @brief Default constructor for Component.
+   *
+   * Initializes a new Component with default values.
+   */
   Component();
-  virtual ~Component() = 0; // Making it abstract
+
+  /**
+   * @brief Pure virtual destructor for Component.
+   *
+   * A pure virtual destructor is needed to make this class abstract.
+   */
+  virtual ~Component() = 0;
+
+  /**
+   * @brief Check if the Component is active.
+   *
+   * @return True if the Component is active, false otherwise.
+   */
+  bool IsActive() const;
+
+  /**
+   * @brief Set the active status of the Component.
+   *
+   * Sets the active status of the Component to the given value.
+   * @param active The new active status of the Component.
+   */
+  void SetActive(bool active);
+
+  /**
+   * @brief Update the Component.
+   *
+   * This method should be overridden by subclasses to provide specific update
+   * behavior.
+   */
+  virtual void Update();
+
+  /**
+   * @brief Check a condition.
+   *
+   * This method should be overridden by subclasses to provide specific
+   * condition checking behavior.
+   */
+  virtual void Condition();
 };
 
 #endif // AVANS_SPCPRJ13_COMPONENT_H
