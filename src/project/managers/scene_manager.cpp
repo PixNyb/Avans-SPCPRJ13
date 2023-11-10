8fb01f74dc4f5dd522e22376bdf5235d8c8ab3a9
Melvin <17225342+ChocoMelvin@users.noreply.github.com>
2023-11-10 10:46:51 +0100

    Added comments for scene manager and some utility methods

--- src/project/managers/scene_manager.cpp
+++ src/project/managers/scene_manager.cpp
@@ -22,3 +22,19 @@ void SceneManager::RenderScene() { currentScene->RenderScene(); }
 std::weak_ptr<Scene> SceneManager::GetScene() { return currentScene; }
 
 bool SceneManager::HasScene() const { return currentScene != nullptr; }
+
+void SceneManager::Update(float deltaTime)
+{
+    if (currentScene != nullptr)
+    {
+        currentScene->Update(deltaTime);
+    }
+}
+void SceneManager::ClearScene()
+{
+    if (currentScene != nullptr)
+    {
+        currentScene->Clear();
+        currentScene = nullptr;
+    }
+}
