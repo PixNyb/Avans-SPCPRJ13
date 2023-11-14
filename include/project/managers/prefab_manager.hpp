3296714cd6cb8c50fa898c06740436f64688935f
Max Groenendaal <mr.groenendaal@student.avans.nl>
2023-11-14 13:35:18 +0100

    Add: PrefabManager

---
+++ include/project/managers/prefab_manager.hpp
@@ -0,0 +1,58 @@
+/**
+ * @file prefab_manager.hpp
+ * @author Max Groenendaal
+ * @brief Manages prefabs
+ * @version 0.1
+ * @date 14/11/2023
+ *
+ * @copyright Copyright (c) 2023
+ */
+
+#ifndef DEFUNBOBENGINE_PREFAB_MANAGER_HPP
+#define DEFUNBOBENGINE_PREFAB_MANAGER_HPP
+
+#include <map>
+#include <string>
+#include "../api/game_object.hpp"
+
+class PrefabManager {
+  private:
+    std::map<std::string, GameObject> prefabs;
+
+  public:
+    PrefabManager();
+
+    /**
+     * @brief Registers a prefab.
+     * @param tag The unique key that can later be used to retrieve the prefab.
+     * @param prefab The prefab that is to be registered.
+     */
+    void RegisterPrefab(std::string tag, GameObject prefab);
+
+    /**
+     * @brief Gets a prefab based on a tag.
+     * @param tag Defines which prefab is meant to be returned.
+     * @return Returns the prefab that matches the provided tag.
+     */
+    GameObject GetPrefab(std::string tag);
+
+    /**
+     * @brief Check if there is a prefab registered with a specified tag.
+     * @param tag Defines which tag to check for.
+     * @return True if there is already a prefab registered by the specified tag.
+     */
+    bool HasPrefab(std::string tag) const;
+
+    /**
+     * @brief Removes a prefab based on the specified tag.
+     * @param tag Specifies which prefab is too be removed.
+     */
+    void RemovePrefab(std::string tag);
+
+    /**
+     * @brief Clears all of the currently registered prefabs.
+     */
+    void ClearPrefabs();
+};
+
+#endif // DEFUNBOBENGINE_PREFAB_MANAGER_HPP
---
+++ src/project/managers/prefab_manager.cpp
@@ -0,0 +1,47 @@
+//
+// Created by maxro on 14/11/2023.
+//
+
+#include "prefab_manager.hpp"
+
+PrefabManager::PrefabManager() : prefabs()
+{}
+
+void PrefabManager::RegisterPrefab(std::string tag, GameObject prefab)
+{
+    prefabs.insert(std::pair(tag, prefab));
+}
+
+GameObject PrefabManager::GetPrefab(std::string tag)
+{
+    auto it = prefabs.find(tag);
+
+    // TODO: Throw and exception.
+    if (it == prefabs.end())
+        return GameObject();
+
+    // TODO: Add copy functionality.
+    return GameObject();
+}
+
+bool PrefabManager::HasPrefab(std::string tag) const
+{
+    auto it = prefabs.find(tag);
+
+    return it != prefabs.end();
+}
+
+void PrefabManager::RemovePrefab(std::string tag)
+{
+    auto it = prefabs.find(tag);
+
+    if (it == prefabs.end())
+        return;
+
+    prefabs.erase(it);
+}
+
+void PrefabManager::ClearPrefabs()
+{
+    prefabs.clear();
+}
