5497d62fba4d94b5318e61024629d73fdf693e9e
RobinPijnappelsAvans <73393404+RobinPijnappelsAvans@users.noreply.github.com>
2023-11-15 15:08:19 +0100

    Doxygen for all src files

--- src/project/graphics/window.cpp
+++ src/project/graphics/window.cpp
@@ -1,3 +1,10 @@
-//
-// Created by Robin Pijnappels on 07/11/2023.
-//
+/**
+ * @file window.cpp
+ * @author Robin Pijnappels
+ * @brief Implementation of the Window class, responsible for managing the game's window and rendering context.
+ * @version 0.1
+ * @date 08/11/2023
+ *
+ * Copyright Copyright (c) 2023
+ *
+ */
