eb8d311af8cf773aa7a3ef06f6d9ed63db7a0aea
RobinPijnappelsAvans <73393404+RobinPijnappelsAvans@users.noreply.github.com>
2023-12-11 21:05:23 +0100

    try this

--- src/project/managers/render_manager.cpp
+++ src/project/managers/render_manager.cpp
@@ -152,11 +152,15 @@ void RenderManager::Render(IOFacade &gfx, ShapeRenderer &shapeRenderer, const Po
             // Handling sprite sheets with Animator
             int currentFrameIndex = animatorComponent->GetCurrentFrameIndex();
 
-            int totalColumns = animatorComponent->GetTotalColumns() == 0
-                                   ? 10
-                                   : animatorComponent->GetTotalColumns();
-            int totalRows =
-                animatorComponent->GetTotalRows() == 0 ? 1 : animatorComponent->GetTotalRows();
+            //            int totalColumns = animatorComponent->GetTotalColumns() == 0
+            //                                   ? 10
+            //                                   : animatorComponent->GetTotalColumns();
+            //            int totalRows =
+            //                animatorComponent->GetTotalRows() == 0 ? 1 :
+            //                animatorComponent->GetTotalRows();
+
+            int totalColumns = 10;
+            int totalRows = 1;
 
             gfx.DrawSpriteSheetFrame(spriteComponent->GetSprite(), spriteRect, currentFrameIndex,
                                      totalColumns, totalRows,
