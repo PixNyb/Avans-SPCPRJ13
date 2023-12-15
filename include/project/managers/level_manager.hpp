e7b03f27a7ac670f2c648707ebc86f72a544ff7e
DaanMonk <d.groot3@student.avans.nl>
2023-12-15 19:21:01 +0100

    added a currentlevel

--- include/project/managers/level_manager.hpp
+++ include/project/managers/level_manager.hpp
@@ -60,6 +60,8 @@ class LevelManager
      */
     std::string levelFileExtension;
 
+    int currentLevelID; ///< Resembles the currentLevel
+
     /**
      * @brief Create a json object from a GameObject.
      * @param gameObject The GameObject which is to converted.
@@ -107,6 +109,8 @@ class LevelManager
      * @return The path of where the level is stored.
      */
     std::string SaveLevel(std::string &directory, std::string &filename);
+
+    int GetCurrentLevelID() { return currentLevelID; }
 };
 
 #endif // DEFUNBOBENGINE_LEVEL_MANAGER_HPP
