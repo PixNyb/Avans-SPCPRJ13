e880c2be6b34968e3bac74a63373b1b3c1bd25af
Martijn-V1 <82821300+Martijn-V1@users.noreply.github.com>
2023-12-20 11:42:56 +0100

    Created doxygen comments

--- include/project/graphics/particle_type.hpp
+++ include/project/graphics/particle_type.hpp
@@ -1,10 +1,22 @@
+/**
+ * @file particle_type.hpp
+ *  @author Martijn Vermeer
+ * @brief Defines the ParticleType enum for specifying types of particles.
+ * @version 0.1
+ * @date 2023-20-12
+ */
+
 #ifndef DEFUNBOBENGINE_PARTICLE_TYPE_HPP
 #define DEFUNBOBENGINE_PARTICLE_TYPE_HPP
 
+/**
+ * @enum ParticleType
+ * @brief Enumerates the different types of particles.
+ */
 enum ParticleType
 {
-    Rain,
-    Confetti
+    Rain,    ///< Represents rain particles.
+    Confetti ///< Represents confetti particles.
 };
 
 #endif // DEFUNBOBENGINE_PARTICLE_TYPE_HPP
